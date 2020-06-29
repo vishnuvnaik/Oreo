089f948a2ed5a3b87ef79e1440797b848f4cf893
= <=>
2020-06-29 23:12:35 +0530

    [Vishnu]Add.added webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const webpack = require("webpack");
+var CopyWebpackPlugin = require("copy-webpack-plugin");
 module.exports = {
   entry: "./src/index.js",
   module: {
@@ -7,6 +8,56 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: ["babel-loader"],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+            // options: {
+            //   modules: true,
+            // },
+          },
+        ],
+      },
+      {
+        test: /\.less$/,
+        use: [
+          {
+            loader: "style-loader",
+          },
+          {
+            loader: "css-loader",
+          },
+          {
+            loader: "less-loader",
+          },
+        ],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[path][name]-[hash:8].[ext]",
+            },
+          },
+        ],
       },
     ],
   },
@@ -18,9 +69,14 @@ module.exports = {
     publicPath: "/",
     filename: "bundle.js",
   },
-  plugins: [new webpack.HotModuleReplacementPlugin()],
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [{ from: "src/Assets", to: "Assets" }],
+    }),
+  ],
   devServer: {
     contentBase: "./dist",
+    historyApiFallback: true,
     hot: true,
   },
 };
