69fe4791a926261cb22e25c8bb3c4fbdf61546fb
= <=>
2020-06-29 23:16:42 +0530

    [Vishnu]Add.added src

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import App from "./App";
 const title = "React with Webpack and Babel";
 
-ReactDOM.render(<div>{title}</div>, document.getElementById("app"));
+ReactDOM.render(<App />, document.getElementById("app"));
 module.hot.accept();
