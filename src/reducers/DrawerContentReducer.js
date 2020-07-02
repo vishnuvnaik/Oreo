1b7711089d5ee09e2e31e9f0f2e3ba83f7861417
= <=>
2020-07-03 01:00:27 +0530

    [Vishnu]Add.added reducers for drawer content

---
+++ src/reducers/DrawerContentReducer.js
@@ -0,0 +1,23 @@
+import { USER_SHOW, HOME_SHOW } from "../constants/actionTypes";
+const initialstate = {
+  drawershow: false,
+  drawerslide: false,
+};
+export default (state = { drawershow: true, drawerslide: false }, action) => {
+  switch (action.type) {
+    case "USER_SHOW":
+      return {
+        ...state,
+        drawershow: false,
+        drawerslide: true,
+      };
+    case "HOME_SHOW":
+      return {
+        ...state,
+        drawershow: true,
+        drawerslide: true,
+      };
+    default:
+      return state;
+  }
+};
