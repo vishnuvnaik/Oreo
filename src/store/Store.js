a78b16c3fa25df72bebb0eb619458c9490f94107
= <=>
2020-07-05 03:43:08 +0530

    [Vishnu]Add.added store for redux

--- src/store/Store.js
+++ src/store/Store.js
@@ -1,10 +1,14 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import logger from "redux-logger";
 import DrawerContentReducer from "../reducers/DrawerContentReducer";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 const rootReducer = combineReducers({
   DrawerContentReducer,
 });
 
-const store = createStore(rootReducer, applyMiddleware(logger));
+const store = createStore(
+  DrawerContentReducer,
+  composeWithDevTools(applyMiddleware(logger))
+);
 export default store;
