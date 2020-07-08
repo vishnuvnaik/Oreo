aa0c0b01ddb9598bab0a707fdb6f7d970952f5be
= <=>
2020-07-08 12:09:08 +0530

    [Vishnu]Refactor.added action types for redux part in product details

--- src/constants/actionTypes.js
+++ src/constants/actionTypes.js
@@ -1,2 +1,5 @@
 export const USER_SHOW = "USER_SHOW";
 export const HOME_SHOW = "HOME_SHOW";
+export const DESCRIPTION = "DESCRIPTION";
+export const REVIEW = "REVIEW";
+export const ABOUT = "ABOUT";
