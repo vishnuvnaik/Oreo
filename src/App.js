4d880e67c8ba98ed9578fb7ab81a280530ea7930
= <=>
2020-07-08 12:10:06 +0530

    [Vishnu]Refactor.implemented redux

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Router, Switch, Route } from "react-router-dom";
+import history from "./history";
 import Dashboard from "./Components/Dashboard/Dashboard.jsx";
 import Product from "./Components/product/Product.jsx";
-import ProductList from "./Components/productList/ProductList";
-import ProductDetails from "./Components/productDetails/ProductDetails";
+import ProductList from "./Components/productList/ProductList.jsx";
+import ProductDetails from "./Components/productDetails/ProductDetails.jsx";
 
 class App extends Component {
   render() {
     return (
-      <Router>
+      <Router history={history}>
         <Switch>
           <Route path="/" component={Dashboard} />
-         
           <Route path="/product" component={Product} />
           <Route path="/productList" component={ProductList} />
           <Route path="/productDetails" component={ProductDetails} />
--- src/store/Store.js
+++ src/store/Store.js
@@ -2,9 +2,10 @@ import { createStore, combineReducers, applyMiddleware } from "redux";
 import logger from "redux-logger";
 import DrawerContentReducer from "../reducers/DrawerContentReducer";
 import { composeWithDevTools } from "redux-devtools-extension";
-
+import ProductDetailsReducer from "../reducers/ProductDetailsReducer";
 const rootReducer = combineReducers({
   DrawerContentReducer,
+  ProductDetailsReducer,
 });
 
 const store = createStore(
