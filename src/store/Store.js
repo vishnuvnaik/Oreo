17420006963ba0a8345950e53cf23d73083583cd
= <=>
2020-07-16 01:01:11 +0530

    [Vishnu]Removed unwanted and commented codes

--- src/store/Store.js
+++ src/store/Store.js
@@ -2,10 +2,6 @@ import { createStore, combineReducers, applyMiddleware } from "redux";
 import logger from "redux-logger";
 import Reducer from "../reducers/Reducer";
 import { composeWithDevTools } from "redux-devtools-extension";
-//import ProductDetailsReducer from "../reducers/ProductDetailsReducer";
-// const rootReducer = combineReducers({
-//   DrawerContentReducer,
-// });
 
 const store = createStore(
   Reducer,
